51dd33c628874e95383e20fd7ce284177c348d5d
Ilya Minyaev <ayli.veaynim@gmail.com>
2018-02-12 16:24:32 +0200

    Downgrading winston

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,17 @@ var winston = require('winston');
 var router = express.Router();
 
 winston.loggers.add('city', {
-    format: winston.format.combine(
-        winston.format.label({ label: 'City' }),
-        winston.format.timestamp(),
-        winston.format.simple()
-    ),
-    transports: [
-        new winston.transports.Console({
-            colorize: true,
-            timestamp: true
-        }),
-        new winston.transports.File({
-            filename: './logs/city.log',
-            options: {
-                flags: 'w'
-            }
-        })
-    ]
+    console: {
+        colorize: true,
+        timestamp: true,
+        label: 'City'
+    },
+    file: {
+        filename: './logs/city.log',
+        options: {
+            flags: 'w'
+        }
+    }
 });
 
 router.use('/oulu/api/city', require('./city'));
